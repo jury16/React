ccbe790d67c98398b03e93bf71ccb338174b8470
jury16 <76671367+jury16@users.noreply.github.com>
2021-07-28 20:12:05 -0400

    commit changes

---
+++ Ishop/Ishop.js
@@ -0,0 +1,28 @@
+var Ishop = React.createClass({
+
+    displayName: 'IShop',
+    getDefaultProps: function() {
+        return {
+            shopName: "Shop name not found",
+            productsArr: [{ id: 0, brand: 'not found', price: 'not found', url: 'not found', count: 'not found' }]
+        }
+    },
+    render: function() {
+        var carsArr = this.props.cars.map((item) => {
+            return React.DOM.tr({ key: item.code, className: 'IshopFrame' },
+                React.DOM.td({ className: 'IshopText' }, item.brand),
+                React.DOM.td({ className: 'IshopText _number' }, item.code),
+                React.DOM.td({ className: 'IshopText' }, React.DOM.img({ className: 'IshopImg', src: item.url, alt: `${item.brand}` })),
+                React.DOM.td({ className: 'IshopText _number' }, item.count));
+        });
+        const headTable = ["brand", "code", "picture", "quantity"].map((item) => {
+            return React.DOM.td({ key: item, className: 'IshopTableHead' }, item);
+        })
+        console.log(carsArr);
+        return React.DOM.table({ className: 'IshopFrame' },
+            React.DOM.caption({ className: 'IshopName' }, this.props.shopName),
+            React.DOM.thead({ className: 'IshopTableHead' },
+                React.DOM.tr({}, headTable)),
+            React.DOM.tbody({ className: '' }, carsArr))
+    },
+});
