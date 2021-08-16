9e69c2130b915d51323a7105dc726e26edc0a2da
jury16 <76671367+jury16@users.noreply.github.com>
2021-08-15 21:51:54 -0400

    add onChange() to elements

--- Ishop_3/ishop_3/src/ProductCard.js
+++ Ishop_3/ishop_3/src/ProductCard.js
@@ -25,8 +25,17 @@ class ProductCard extends React.Component{
       avaliableChange = () => {
           this.setState()
       }
-      validatingName = (EO) =>{
+      validBrand = (EO) =>{
              }
+      validID = (EO) =>{
+             }
+      validUrl = (EO) =>{
+             }
+      validQuantity = (EO) =>{
+             }
+      validPrice = (EO) =>{
+             }
+             
       
     render(){
         
@@ -47,26 +56,26 @@ class ProductCard extends React.Component{
                 <tbody className=''> 
                     <tr className=''>
                         <td><span className=''>Brand:</span></td>
-                        <td><input className='ProductCardText _input' type="text" id="Brand"  defaultValue={this.props.productCard.brand} onChange={this.validatingName}></input></td>
+                        <td><input className='ProductCardText _input' type="text" id="Brand"  defaultValue={this.props.productCard.brand} onChange={this.validBrand}></input></td>
                     </tr>
                     
                     <tr>
                     <td><span className=''>Id:  </span></td>
-                    <td><input className='ProductCardText _input' type="text" id="Id"  value={this.props.productCard.code} onChange={this.validatingName}></input></td>
+                    <td><input className='ProductCardText _input' type="text" id="Id"  value={this.props.productCard.code} onChange={this.validID}></input></td>
                     </tr> 
                     <tr>
                     <td><span className=''>URL:</span> </td>
-                    <td><input className='ProductCardText _input' type="text" id="URL"  value= {this.props.productCard.url} onChange={this.validatingName}></input></td>
+                    <td><input className='ProductCardText _input' type="text" id="URL"  value= {this.props.productCard.url} onChange={this.validUrl}></input></td>
                     </tr>    
     
                     <tr>
                     <td><span className=''>Quantity: </span></td>
-                    <td><input className='ProductCardText _input' type="text" id="Quantity"  value={this.props.productCard.count} onChange={this.validatingName}></input></td>
+                    <td><input className='ProductCardText _input' type="text" id="Quantity"  value={this.props.productCard.count} onChange={this.validQuantity}></input></td>
 
                     </tr>    
                     <tr>
                     <td><span className=''>Price: </span></td>
-                    <td><input className='ProductCardText _input' type="text" id="Price"  value={this.props.productCard.price} onChange={this.validatingName}></input></td>
+                    <td><input className='ProductCardText _input' type="text" id="Price"  value={this.props.productCard.price} onChange={this.validPrice}></input></td>
                     </tr>       
                     <tr className={displayButton}>
                         <td><input className='ProductCardText _inputButton ' type="button" value="save" /> 
