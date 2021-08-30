51afce4f716c787d7e687531897e516eaf47f908
jury16 <76671367+jury16@users.noreply.github.com>
2021-08-30 10:20:00 -0400

    change reg exp

--- br2jsx/src/BR2JSX.js
+++ br2jsx/src/BR2JSX.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./BR2JSX.css";
 const BR2JSX = (props) => {
     const arr = [];
-    props.text.split(/<br\s{0,1}\/{0,1}>/).forEach((item, index) => {
+    props.text.split(/<br\s?\/?>/).forEach((item, index) => {
       arr.push(item, (<br key={index}/>));
     });
     arr.pop();
