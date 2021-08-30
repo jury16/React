280aba009b8f4669fb95e1f858256f1c39c999a7
jury16 <76671367+jury16@users.noreply.github.com>
2021-08-30 10:12:32 -0400

    change to different br's

--- br2jsx/src/index.js
+++ br2jsx/src/index.js
@@ -2,6 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import BR2JSX from "./BR2JSX";
 
-let text = "first<br/>second<br/>third<br/>last";
+let text = "first<br />second<br>third<br/>last";
 
 ReactDOM.render(<BR2JSX text={text} />, document.getElementById("root"));
