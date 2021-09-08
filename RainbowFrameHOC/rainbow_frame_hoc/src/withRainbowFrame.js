1a1892a2aa838031cce6342cd834995be1b6c851
jury16 <76671367+jury16@users.noreply.github.com>
2021-09-07 22:10:38 -0400

    changed the file

--- RainbowFrameHOC/rainbow_frame_hoc/src/withRainbowFrame.js
+++ RainbowFrameHOC/rainbow_frame_hoc/src/withRainbowFrame.js
@@ -3,29 +3,13 @@ import React from "react";
 const withRainbowFrame = (colors) => {
     return (Comp) => {
         return props =>{
-            let border;
+            let border = <Comp {...props}/>;
             colors.forEach((item, index) => {
-                if (!(index)) {
-                border = (
-                    <div
-                    style={{
-                        textAlign: "center",
-                        border: "5px " + item + " solid",
-                        margin: "10px",
-                    
-                    }}
-                    >
-                    <Comp {...props}/>
-                    </div>
-                );
-                } 
-                else {
                 border = (
                     <div style={{ border: "5px " + item + " solid", margin: "10px" }}>
                     {border}
                     </div>
                 );
-                }
             });
             return <div className="withRainbowFrame">{border}</div>;
         }       
